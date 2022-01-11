a8b360c7dcda185f26d5aaa306b31e8766ccfda1
Ken0806 <ken1min86@gmail.com>
2022-01-11 23:42:33 +0900

    Ruby

---
+++ Backend/言語/Ruby/LeetCode/Trees/maximux_depth_of_binary_tree.rb
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def max_depth(root)
+  depth = 0
+  children = [root]
+  while children.any?
+    depth += 1
+    clone_of_children = children.clone
+    children = []
+    clone_of_children.each do |child|
+      children.push child&.left, child&.right
+    end
+    children.compact!
+  end
+  depth
+end
