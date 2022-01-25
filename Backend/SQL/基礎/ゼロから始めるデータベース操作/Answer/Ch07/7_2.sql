d208b71f8b58c39313d2cf0e8cf23a97ea20dec2
Ken0806 <ken1min86@gmail.com>
2022-01-25 18:43:55 +0900

    SQL

---
+++ Backend/SQL/基礎/ゼロから始めるデータベース操作/Answer/Ch07/7_2.sql
@@ -0,0 +1,9 @@
+  SELECT COALESCE(TS.tenpo_id, '不明'),
+         COALESCE(TS.tenpo_mei, '不明'),
+         TS.tenpo_mei,
+         S.shohin_id,
+         S.shohin_mei,
+         S.hanbai_tanka
+    FROM TenpoShohin TS RIGHT OUTER JOIN Shohin S
+      ON TS.shohin_id = S.shohin_id
+ORDER BY tenpo_id;
